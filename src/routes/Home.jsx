5245ba8ca07f5933d13c4e7f516d3b3475772a90
Gabriel <ggralowrech@gmail.com>
2025-02-21 08:06:37 -0300

    Coletando dados do Formulário

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [title, setTitle] = useState();
+  const [date, setDate] = useState();
+  const [image, setImage] = useState();
+  const [color, setColor] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault;
+
+    const eventObject = {
+      title,
+      date,
+      image,
+      color,
+    };
+  };
+
   return (
     <div>
       <h2 className="text-center text-3xl mb-4 font-bold">
         Monte a sua contagem regressiva!
       </h2>
-      <form className="flex flex-col gap-4 max-w-[400px] my-0 mx-auto">
+      <form
+        className="flex flex-col gap-4 max-w-[400px] my-0 mx-auto"
+        onSubmit={handleSubmit}
+      >
         <label className="flex flex-col gap-2">
           <span className="font-bold">Título: </span>
           <input
@@ -14,6 +33,8 @@ const Home = () => {
             name="title"
             placeholder="Digite o título do evento..."
             className="p-2 rounded-md border border-[#AAA]"
+            onChange={(e) => setTitle(e.target.value)}
+            required
           />
         </label>
         <label className="flex flex-col gap-2">
@@ -22,6 +43,8 @@ const Home = () => {
             type="date"
             name="date"
             className="p-2 rounded-md border border-[#AAA]"
+            onChange={(e) => setDate(e.target.value)}
+            required
           />
         </label>
         <label className="flex flex-col gap-2">
@@ -31,6 +54,7 @@ const Home = () => {
             name="image"
             placeholder="Insira a URL da imagem..."
             className="p-2 rounded-md border border-[#AAA]"
+            onChange={(e) => setImage(e.target.value)}
           />
         </label>
         <label className="flex flex-col gap-2">
@@ -39,6 +63,8 @@ const Home = () => {
             type="color"
             name="color"
             className="p-0.5 rounded-md border border-[#AAA]"
+            onChange={(e) => setColor(e.target.value)}
+            required
           />
         </label>
         <input
